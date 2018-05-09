5cd1999ba5ab3df880782708c3233ec06517c63b
DESKTOP-DR580PQ\midav <midav1991@gmail.com>
2018-05-09 20:04:02 +0300

    Del Film

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -8,18 +8,20 @@ import {Response} from '@angular/http';
   templateUrl: './films.component.html',
   styleUrls: ['./films.component.css']
 })
+
 export class FilmsComponent {
   filmSearch = {};
   films = [];
   flagFilm = '';
   flagFil = false;
+
   constructor(private filmsService: FilmsService) { }
+
   searchFilms(form: NgForm) {
     this.filmsService
       .getFilms(form.value.nameFilm)
       .subscribe((responce: Response) => {
         const data = responce.json();
-        //console.log(data);
         if (data.Response !== 'False') {
           this.filmSearch = data;
           this.flagFilm = 'yes';
@@ -29,11 +31,14 @@ export class FilmsComponent {
         }
       });
   }
-  addFilm() {
 
+  addFilm() {
     this.films.push(this.filmSearch);
     if(this.films.length > 0)
       this.flagFil = true;
     //console.log(this.films);
+  }
+  delFilms(id: number) {
+    this.films.splice(id, 1);
   }
 }
